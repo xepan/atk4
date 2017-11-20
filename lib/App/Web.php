8464fced483f88a867dd8a6fba8f4daaa60f495f
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-11-20 16:19:40 +0530

    strtotime bug solved for windows

--- lib/App/Web.php
+++ lib/App/Web.php
@@ -36,7 +36,7 @@ class App_Web extends App_CLI {
 
     // {{{ Start-up
     function __construct($realm=null,$skin='default',$options=array()){
-        $this->start_time=time()+microtime();
+        $this->start_time=strotime(time());
 
         $this->skin=$skin;
         try {
